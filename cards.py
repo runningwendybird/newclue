8ae93d7c6ef7c9a3deff7647a5025f9f036e1958
Hayley Denbraver <runningwendybird@gmail.com>
2015-01-23 23:06:13 -0800

    Created all the card instances.

---
+++ cards.py
@@ -0,0 +1,28 @@
+from clue_class import Card
+
+# Weapons
+knife = Card("knife", "weapon")
+revolver = Card("revolver", "weapon")
+candlestick = Card("candlestick", "weapon")
+lead_pipe = Card("lead pipe", "weapon")
+rope = Card("rope", "weapon")
+wrench = Card("wrench", "weapon")
+
+# Rooms
+kitchen = Card("kitchen", "room")
+dining = Card("dining room", "room")
+conservatory = Card("conservatory", "room")
+library = Card("library", "room")
+study = Card("study", "room")
+billards = Card("billiard", "room")
+hall = Card("hall", "room")
+ballroom = Card("ballroom", "room")
+lounge = Card("lounge", "room")
+
+# Suspects
+green = Card("Mr. Green", "suspect")
+peacock = Card("Mrs. Peacock", "suspect")
+plum = Card("Prof. Plum", "suspect")
+scarlet = Card("Miss Scarlet", "suspect")
+white = Card("Mrs. White", "suspect")
+mustard = Card("Col. Mustard", "suspect")
